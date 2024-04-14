1fdca35bd53b39f6791c6d8858cb7547f58c287d
Gimhani Chathurika Kumari <162095452+gimchathuu@users.noreply.github.com>
2024-04-14 18:23:51 +0530

    Update index.html

--- index.php
+++ index.php
@@ -43,8 +43,42 @@
             </div>
             
             <br>
-            <?php 
-            include 'footer.html';
-             ?>
+            <div class="footer" id="footer">
+                
+		<div class="content1">
+			<div class="footer-grids">
+				<div class="footer one">
+				<div class="content">
+			<h3>About GiftNest</h3>
+			<p> GiftNest is not your typical online gift shop. We operate by appointment only, 
+				ensuring that every client and order receives the personalized care and attention they deserve.
+				At GiftNest, we understand the importance of making each occasion special, and that's why we go above and beyond to craft unforgettable gifts tailored to our clients' preferences. Whether it's a birthday, anniversary, or any other celebration, 
+				we are dedicated to making your gift-giving experience extraordinary.
+				We're all about turning your gifting dreams into cute realities. Let's make every occasion extra sweet with GiftNest!
+			</p></div>
+				</div>
+				<div class="footer two">
+				<h3>Follow Us</h3>
+			<ul>
+				<li><a class="fb" href="#"><i> <img src="Images\social2.png"/></i><p>Like us on Facebook</p></a></li>
+				<li><a class="fb1" href="#"><i><img src="Images\social1.png"/></i><p>Follow us on Twitter</p></a></li>
+				<li><a class="fb2" href="#"><i><img src="Images\social3.png"/></i><p>Follow us on Instagram</p></a></li>
+			</ul>
+				</div>
+				<div class="footer three">
+				<h3>Contact Information</h3>
+			<ul>
+				<li><img src="Images\Location .png"/> <p>No 52/2,<span>GiftNest,</span>Colombo.</p>  </li>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d776293.8372779922!2d-74.72142612166162!3d40.53279969225431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6f18963519539b99%3A0xdb912a318ca8f1c0!2sThe%20Giftnest!5e0!3m2!1sen!2slk!4v1712608817596!5m2!1sen!2slk" width="140" height="90" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" display="flex"></iframe>
+				<li><img src="Images\phone.png"/> <p>+9471 345 571 9</p>  </li>
+				<li><img src="Images\email.png"/><a href="giftnest@example.com"><p>contact@sithma.com</p></a> </li>
+			</ul>
+				</div>
+			</div>
+			<div class="copy-right-grids">
+			<p class="footer-gd">&#169; 2024 All rights reserved. Designed By Gimhani ICT/2021/059</p>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
